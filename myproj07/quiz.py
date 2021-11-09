a0f0a4e586c4a5aa8fd4d46fb8910e5ddd1c680e
Chinseok Lee <me@askcompany.kr>
2021-11-09 17:14:57 +0900

    과제 가이드 추가

--- myproj07/quiz.py
+++ myproj07/quiz.py
@@ -3,21 +3,29 @@ Quiz: 지정 조건의 인자만 처리하기
 filter_fn에서 True로 판정된 인자는 alter_value 값으로 대체해봅시다.
 """
 
-# fmt: off
 
-def mysum2(x, y):
-    return x + y + 10
+def myfilter(filter_fn, alter_value):
+    def wrap(fn):
+        def inner(*args):
+            # TODO : new_args를 적절히 구성해주세요.
+            # new_args는 리스트 및 튜플 자료구조가 가능합니다.
+            new_args = args
+            return fn(*new_args)
 
-def mysum3(x, y, z):
-    return x + y + z + 10
+        return inner
 
-# 가변인자
-def mysum(x, y, *args):
-    # args is tuple
-    print("args :", args)
-    return x + y + sum(args) + 10
+    return wrap
 
-print(mysum())
-print(mysum(1))
-print(mysum(1, 2))
-print(mysum(1, 2, 3))
+
+@myfilter(lambda i: i % 2 == 0, 0)
+def mysum(a, b, c, d, e):
+    return a + b + c + d + e
+
+
+@myfilter(lambda i: i % 2 == 0, 1)
+def mymultiply(a, b, c, d, e):
+    return a * b * c * d * e
+
+
+print(mysum(1, 2, 3, 4, 5))  # 9
+print(mymultiply(1, 2, 3, 4, 5))  # 15
