67973553361d3381688e2d84254a9c2608497db1
Chinseok Lee <me@askcompany.kr>
2021-11-11 17:01:30 +0900

    TELEGRAM_TOKEN 환경변수 참조토록 개선

--- myproj09/telegram_mybot.py
+++ myproj09/telegram_mybot.py
@@ -1,9 +1,16 @@
+import os
+import sys
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import tasks
 
-token = ""
-updater = Updater(token=token, use_context=True)
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+if TELEGRAM_TOKEN is None:
+    print("TELEGRAM_TOKEN 환경변수를 지정해주세요.", file=sys.stderr)
+    sys.exit(1)  # 종료 상탯값을 1로 지정하고, 프로그램 종료
+
+updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
+
 
 def start(update, context):
     """
@@ -13,6 +20,7 @@ def start(update, context):
         chat_id=update.effective_chat.id,
         text="안녕. 나는 allieusbot이야. 만나서 반가워.")
 
+
 def echo(update, context):
     received_text: str = update.message.text
 
@@ -21,11 +29,12 @@ def echo(update, context):
     elif tasks.naver_search.check_available(received_text):
         response_text = tasks.naver_search.make_response(received_text)
     else:
-        response_text = "지원하는 명령입니다."
+        response_text = "지원하지 않는 명령입니다."
 
     context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=response_text)
+
 
 start_handler = CommandHandler("start", start)
 dispatcher.add_handler(start_handler)
