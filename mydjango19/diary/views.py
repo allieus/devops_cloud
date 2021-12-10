028758d1141435211381837d48690a051d808fe8
Chinseok Lee <me@askcompany.kr>
2021-12-10 14:50:55 +0900

    comment_new/comment_edit 뷰 구현

--- mydjango19/diary/views.py
+++ mydjango19/diary/views.py
@@ -1,9 +1,9 @@
 from django.contrib import messages
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse, Http404
+from django.shortcuts import render, redirect, get_object_or_404
 
-from diary.forms import PostForm
-from diary.models import Post
+from diary.forms import PostForm, CommentForm
+from diary.models import Post, Comment
 
 
 def tag_detail(request: HttpRequest, tag_name: str) -> HttpResponse:
@@ -29,7 +29,13 @@ def post_list(request: HttpRequest) -> HttpResponse:
 
 
 def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
-    post = Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
+
+    # try:
+    #     post = Post.objects.get(pk=pk)  # DoesNotExist 예외
+    # except Post.DoesNotExist:
+    #     raise Http404  # 예외 발생
+
     comment_list = post.comment_set.all()
     tag_list = post.tag_set.all()
 
@@ -59,7 +65,7 @@ def post_new(request: HttpRequest) -> HttpResponse:
 
 def post_edit(request: HttpRequest, pk: int) -> HttpResponse:
     # 아래 코드는 ModelForm에 한해서 동작하는 코드.
-    post = Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
 
     if request.method == "POST":
         form = PostForm(request.POST, request.FILES, instance=post)
@@ -71,5 +77,43 @@ def post_edit(request: HttpRequest, pk: int) -> HttpResponse:
         form = PostForm(instance=post)
 
     return render(request, "diary/post_form.html", {
+        "form": form,
+    })
+
+
+# /diary/100/comments/new/
+def comment_new(request: HttpRequest, post_pk: int) -> HttpResponse:
+    post = get_object_or_404(Post, pk=post_pk)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST, request.FILES)
+        if form.is_valid():
+            # form.cleaned_data  # 유효성 검사에 통과한 값들 (dict)
+            comment = form.save(commit=False)
+            # comment.post_id = post_pk  # FK를 직접 채우진 않습니다.
+            comment.post = post
+            comment.save()
+            return redirect("diary:post_detail", post_pk)
+    else:
+        form = CommentForm()
+
+    return render(request, "diary/comment_form.html", {
+        "form": form,
+    })
+
+
+# /diary/100/comments/20/edit/
+def comment_edit(request: HttpRequest, post_pk: int, pk: int) -> HttpResponse:
+    comment = get_object_or_404(Comment, pk=pk)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST, request.FILES, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect("diary:post_detail", post_pk)
+    else:
+        form = CommentForm(instance=comment)
+
+    return render(request, "diary/comment_form.html", {
         "form": form,
     })
