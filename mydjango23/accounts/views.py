9825763065b643ccc22d9eec8466643bcd948ba2
Chinseok Lee <me@askcompany.kr>
2021-12-16 17:12:58 +0900

    회원가입폼/로그인폼 커스텀

--- mydjango23/accounts/views.py
+++ mydjango23/accounts/views.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
 from PIL import Image
+
+from accounts.forms import LoginForm, SignupForm
 
 
 def profile_image(request: HttpRequest) -> HttpResponse:
@@ -19,6 +18,7 @@ def profile_image(request: HttpRequest) -> HttpResponse:
 
 
 login = LoginView.as_view(
+    form_class=LoginForm,
     template_name="accounts/login_form.html",
 )
 
@@ -26,12 +26,12 @@ login = LoginView.as_view(
 # 새로운 User 인스턴스를 만드는 것.
 def signup(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST, request.FILES)
+        form = SignupForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect("accounts:login")
     else:
-        form = UserCreationForm()
+        form = SignupForm()
 
     return render(
         request,
