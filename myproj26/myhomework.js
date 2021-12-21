c800a69a9a4168d4b93f3bb8125e860c6924de0d
Chinseok Lee <me@askcompany.kr>
2021-12-21 13:30:41 +0900

    added myproj26/myhomework.js

--- myproj26/myhomework.js
+++ myproj26/myhomework.js
@@ -1,3 +1,6 @@
+// npm install readline-sync
+
+const { question } = require("readline-sync");
 
 const animal_names = [
     "cat",
@@ -11,19 +14,41 @@ const animal_names = [
     "wolf",
 ];
 
-// TODO: 현재 timestamp
+// const random_animal_names = animal_names
+//     .map((name) => ({
+//         name,
+//         value: Math.random(),
+//     }))
+//     .sort((obj_a, obj_b) => {
+//         return obj_a.value - obj_b.value;
+//     })
+//     .map(obj => {
+//         return obj.value;
+//     });
 
-// TODO: shuffle
+const shuffled_animal_names = animal_names
+    .map((name) => ({
+        name,
+        value: Math.random(),
+    }))
+    .sort((obj_a, obj_b) => {
+        return obj_a.value - obj_b.value;
+    })
+    .map(({ name }) => name);
 
-// TODO: slicing
+const begin_time = new Date().getTime();  // float
+let ok_counter = 0;
 
-// TODO: input 받기
-//   readline-sync 라이브러리를 설치
-//   소스코드가 있는 폴더까지 이동해서 !!!
-//   npm install readline-sync
+for (const animal_name of shuffled_animal_names.slice(0, 5)) {
+    const line = question(`${animal_name} >>> `);
+    if (line === animal_name) {
+        ok_counter++;
+    }
+}
 
-const { question } = require("readline-sync");
+const end_time = new Date().getTime();   // float
 
-// string
-const number = question("Enter a number : ");
-console.log(number);
+const time = end_time - begin_time;
+console.log(`총 ${parseInt(time / 1000)}초가 소요되었습니다.`);
+
+console.log(`총 ${ok_counter}회 맞추셨습니다.`);
