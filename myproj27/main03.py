83d544551cc2b6722d6226f7cb91772b374b0953
Chinseok Lee <me@askcompany.kr>
2021-12-22 11:02:44 +0900

    main03.py에 인코딩 옵션 추가

--- myproj27/main03.py
+++ myproj27/main03.py
@@ -1,6 +1,8 @@
 import json
 
-with open("melon_data.json", "rt") as f:
+# 한글 윈도우는 기본 인코딩이 cp949이기에
+# open을 통한 text 파일 열기에서는 필히 encoding을 지정해주어야 합니다.
+with open("melon_data.json", "rt", encoding="utf-8") as f:
     song_list = json.loads(f.read())
 
 song_list.sort(key=lambda song: song['like'], reverse=True)
