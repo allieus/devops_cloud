9855cae03bad41f13f43c9b7ec1a1235be06222f
Chinseok Lee <me@askcompany.kr>
2021-12-23 11:56:55 +0900

    카운터 컴포넌트 예시 추가

--- myreact29/src/App.js
+++ myreact29/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+function Counter() {
+  const [value, setValue] = useState(10);
+  const [color, setColor] = useState('red');
+
+  const handleClick = () => {
+    console.log(`clicked`);
+    setValue(value + 1);
+    setColor(value % 2 === 0 ? 'green' : 'red');
+  };
+
+  return (
+    <div
+      style={{
+        backgroundColor: color,
+        width: 100,
+        height: 100,
+        display: 'inline-block',
+        margin: 5,
+        borderRadius: 50,
+        textAlign: 'center',
+        userSelect: 'none',
+      }}
+      onClick={handleClick}
+    >
+      <span>{value}</span>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>안녕 리액트</h1>
+      <Counter />
+      <Counter />
+      <Counter />
     </div>
   );
 }
