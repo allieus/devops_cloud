f8d5fcfbe9a506a79ded4e40e6c16d72f6c8c102
Chinseok Lee <me@askcompany.kr>
2021-12-24 14:32:05 +0900

    TopNav 컴포넌트 구현

--- myreact30/src/App.js
+++ myreact30/src/App.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
+import TopNav from "components/TopNav";
 import PageAbout from "pages/PageAbout";
 import PageCounter from "pages/PageCounter";
 
 function App() {
+  const [pageName, setPageName] = useState("about");
+
+  // const changePageName = (pageName) => {
+  //   setPageName(pageName);
+  // };
+
   return (
-    <>
-      <PageAbout />
-      <PageCounter />
-    </>
+    <div>
+      <h1>이진석의 리액트</h1>
+      {/* <button onClick={changePageName}>페이지 토글</button> */}
+      <TopNav changePageName={setPageName} />
+      {pageName === "about" && <PageAbout />}
+      {pageName === "counter" && <PageCounter />}
+    </div>
   );
 }
 
