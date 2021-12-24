095b29d05adc387d8b589f8be71217367d854d51
Chinseok Lee <me@askcompany.kr>
2021-12-24 16:09:41 +0900

    플레이리스트 컴포넌트 과제 풀이

--- myreact30/src/pages/PagePlaylist.jsx
+++ myreact30/src/pages/PagePlaylist.jsx
@@ -1,5 +1,33 @@
+import { useState } from 'react';
+import ReactPlayer from 'react-player';
+import songList from 'assets/song_list.json';
+import './PagePlaylist.css';
+
 function PagePlaylist() {
-  return <div>플레이 리스트</div>;
+  const [videoUrl, setVideoUrl] = useState();
+
+  // const showVideoPlayer = (videoUrl) => setVi
+
+  return (
+    <div>
+      <h2>플레이 리스트</h2>
+
+      <div className="player-wrap">
+        <ul className="song-list">
+          {songList.map((song) => (
+            <div className="song" onClick={() => setVideoUrl(song.url)}>
+              <img src={song.thumbnail_url} alt={song.title} />
+              {song.title}
+            </div>
+          ))}
+        </ul>
+        <div className="song-player">
+          {!videoUrl && '재생할 영상을 선택해주세요.'}
+          {videoUrl && <ReactPlayer url={videoUrl} />}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PagePlaylist;
