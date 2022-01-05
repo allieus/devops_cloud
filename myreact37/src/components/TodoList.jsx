5d3ea2bfde7c39e8af4d1a95ca3420f37979767b
Chinseok Lee <me@askcompany.kr>
2022-01-05 14:06:19 +0900

    [myreact37] TodoForm과 커스텀 훅 useFieldValues

--- myreact37/src/components/TodoList.jsx
+++ myreact37/src/components/TodoList.jsx
@@ -1,4 +1,8 @@
+import useFieldValues from 'hooks/useFieldValues';
 import { useState } from 'react';
+import Todo from './Todo';
+import TodoForm from './TodoForm';
+import './TodoList.css';
 
 const INITIAL_STATE = [
   { content: '2022년에는 살 빼기' },
@@ -7,8 +11,8 @@ const INITIAL_STATE = [
 ];
 
 function TodoList() {
-  const [inputText, setInputText] = useState('');
   const [todoList, setTodoList] = useState(INITIAL_STATE);
+  const [fieldValues, handleChange] = useFieldValues();
 
   const removeTodo = (todoIndex) => {
     setTodoList((prevTodoList) =>
@@ -16,41 +20,45 @@ function TodoList() {
     );
   };
 
-  const changedInputText = (e) => {
-    setInputText(e.target.value);
-  };
+  // const changedInputText = (e) => {
+  //   setInputText(e.target.value);
+  // };
 
-  const appendInputText = (e) => {
-    console.log('e.key :', e.key);
-    if (e.key === 'Enter') {
-      // todoList 배열 끝에 inputText를 추가합니다.
-      // inputText를 다시 비웁니다.
-      console.log('inputText :', inputText);
+  // const appendInputText = (e) => {
+  //   console.log('e.key :', e.key);
+  //   if (e.key === 'Enter') {
+  //     // todoList 배열 끝에 inputText를 추가합니다.
+  //     // inputText를 다시 비웁니다.
+  //     console.log('inputText :', inputText);
 
-      // setTodoList 에 함수를 넘기는 것.
-      // todoList 상탯값을 직접 변경하는 것은 아닙니다. (배열의 push를 사용 X)
+  //     // setTodoList 에 함수를 넘기는 것.
+  //     // todoList 상탯값을 직접 변경하는 것은 아닙니다. (배열의 push를 사용 X)
 
-      setTodoList((prevTodoList) => {
-        return [...prevTodoList, { content: inputText }];
-      });
+  //     setTodoList((prevTodoList) => {
+  //       return [...prevTodoList, { content: inputText }];
+  //     });
 
-      setInputText('');
-    }
-  };
+  //     setInputText('');
+  //   }
+  // };
 
   return (
-    <div>
+    <div className="todo-list">
       <h2>Todo List</h2>
 
-      <input
+      <TodoForm handleChange={handleChange} />
+      <hr />
+      {JSON.stringify(fieldValues)}
+
+      {/* <input
         type="text"
         value={inputText}
         onChange={changedInputText}
         onKeyPress={appendInputText}
-      />
+      /> */}
 
       {todoList.map((todo, index) => (
-        <div onClick={() => removeTodo(index)}>{todo.content}</div>
+        <Todo todo={todo} onClick={() => removeTodo(index)} />
       ))}
     </div>
   );
