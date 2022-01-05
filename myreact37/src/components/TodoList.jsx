7bac8a1304410f4dcdaa66825d44cb0ea477bde2
Chinseok Lee <me@askcompany.kr>
2022-01-05 15:06:53 +0900

    [myreact37] 새로운 todo 항목 저장

--- myreact37/src/components/TodoList.jsx
+++ myreact37/src/components/TodoList.jsx
@@ -5,16 +5,16 @@ import TodoForm from './TodoForm';
 import './TodoList.css';
 
 const INITIAL_STATE = [
-  { content: '2022년에는 살 빼기' },
-  { content: '파이썬 익히기' },
-  { content: '리액트 익히기' },
+  { content: '2022년에는 살 빼기', color: 'blue' },
+  { content: '파이썬 익히기', color: 'red' },
+  { content: '리액트 익히기', color: 'red' },
 ];
 
 function TodoList() {
   const [todoList, setTodoList] = useState(INITIAL_STATE);
   const [fieldValues, handleChange, clearFieldValues] = useFieldValues({
     content: '',
-    color: 'Orange',
+    color: 'red',
   });
 
   const removeTodo = (todoIndex) => {
@@ -23,33 +23,29 @@ function TodoList() {
     );
   };
 
-  // const changedInputText = (e) => {
-  //   setInputText(e.target.value);
-  // };
+  const appendTodo = () => {
+    console.log('새로운 todo를 추가하겠습니다.');
 
-  // const appendInputText = (e) => {
-  //   console.log('e.key :', e.key);
-  //   if (e.key === 'Enter') {
-  //     // todoList 배열 끝에 inputText를 추가합니다.
-  //     // inputText를 다시 비웁니다.
-  //     console.log('inputText :', inputText);
+    const todo = { ...fieldValues };
 
-  //     // setTodoList 에 함수를 넘기는 것.
-  //     // todoList 상탯값을 직접 변경하는 것은 아닙니다. (배열의 push를 사용 X)
+    // setter에 값 지정 방식
+    // setTodoList([...todoList, todo]);
 
-  //     setTodoList((prevTodoList) => {
-  //       return [...prevTodoList, { content: inputText }];
-  //     });
+    // setter에 함수 지정 방식
+    setTodoList((prevTodoList) => [...prevTodoList, todo]);
 
-  //     setInputText('');
-  //   }
-  // };
+    clearFieldValues();
+  };
 
   return (
     <div className="todo-list">
       <h2>Todo List</h2>
 
-      <TodoForm fieldValues={fieldValues} handleChange={handleChange} />
+      <TodoForm
+        fieldValues={fieldValues}
+        handleChange={handleChange}
+        handleSubmit={appendTodo}
+      />
       <hr />
       {JSON.stringify(fieldValues)}
 
