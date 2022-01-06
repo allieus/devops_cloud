a54aa2c35e0b3e9240708fec080a38415e3fe4d9
Chinseok Lee <me@askcompany.kr>
2022-01-06 13:17:33 +0900

    [myreact37] review 객체에 id 속성 추가

--- myreact37/src/components/ReviewList.jsx
+++ myreact37/src/components/ReviewList.jsx
@@ -4,13 +4,19 @@ import { useState } from 'react';
 import ReviewForm from './ReviewForm';
 
 const INITIAL_STATE = [
-  { content: '스파이더맨 1 부터 본 사람이면 재미없을 수가 없다', score: 5 },
   {
+    id: 1,
+    content: '스파이더맨 1 부터 본 사람이면 재미없을 수가 없다',
+    score: 5,
+  },
+  {
+    id: 2,
     content:
       '전 스파이더맨 두명이 자책했던 일들이 여기서 어느정도 구원받았다는거에 감사드립니다',
     score: 4,
   },
   {
+    id: 3,
     content:
       '엔드게임을 뛰어넘는 영화가 죽기전에 나올까 생각했었는데.. 2년만에 나왔습니다',
     score: 5,
@@ -25,8 +31,13 @@ function ReviewList() {
   });
   const [isShowReviewForm, setIsShowReviewForm] = useState(false);
 
+  // 새로운 리뷰 저장
   const saveReview = () => {
-    const review = { ...fieldValues };
+    // review는 데이터베이스에 저장하면 id를 할당해줍니다.
+
+    const reviewId = new Date().getTime();
+
+    const review = { ...fieldValues, id: reviewId };
     setReviewList((prevReviewList) => [...prevReviewList, review]);
     clearFieldValues();
     setIsShowReviewForm(false);
