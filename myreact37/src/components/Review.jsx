5ccc0aaece722110bd6d5567ff9ea7ba72ce45d5
Chinseok Lee <me@askcompany.kr>
2022-01-06 13:34:10 +0900

    [myreact37] Review 컴포넌트에 수정/삭제 버튼 추가

--- myreact37/src/components/Review.jsx
+++ myreact37/src/components/Review.jsx
@@ -1,8 +1,25 @@
+import { useState } from 'react';
 import Rating from './Rating';
 
 function Review({ review }) {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <div className="bg-white my-1 p-1 border-2 border-green-600 hover:bg-green-200 cursor-pointer">
+    <div
+      onMouseEnter={() => setShowMenu(true)}
+      onMouseLeave={() => setShowMenu(false)}
+      className="bg-white my-1 p-1 pt-3 border-2 border-green-600  relative"
+    >
+      {showMenu && (
+        <div className="text-xs absolute top-0 right-0">
+          <span className="text-green-700 hover:bg-green-300 cursor-pointer mr-1">
+            수정
+          </span>
+          <span className="text-red-700 hover:bg-red-300 cursor-pointer">
+            삭제
+          </span>
+        </div>
+      )}
       {review.content}
       <Rating score={review.score} />
     </div>
