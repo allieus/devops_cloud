525ca671491fb3f5c43e92d0f4ca3b4144b24b5a
Chinseok Lee <me@askcompany.kr>
2022-01-06 11:30:52 +0900

    [myreact38] Review 삭제 구현

--- myreact37/src/components/Review.jsx
+++ myreact37/src/components/Review.jsx
@@ -1,10 +1,33 @@
+import { useState } from 'react';
 import Rating from './Rating';
 
-function Review({ review }) {
+function Review({ review, handleDelete }) {
+  const [showDeleteButton, setShowDeleteButton] = useState(false);
+
+  const handleClickedDeleteButton = () => {
+    if (handleDelete) {
+      handleDelete(review);
+    } else {
+      console.warn('handleDelete 속성값이 없습니다.');
+    }
+  };
+
   return (
-    <div className="bg-white my-1 p-1 border-2 border-green-600 hover:bg-green-200 cursor-pointer">
+    <div
+      className="p-2 my-3 bg-white border-2 border-green-600 hover:bg-green-200 relative"
+      onMouseEnter={() => setShowDeleteButton(true)}
+      onMouseLeave={() => setShowDeleteButton(false)}
+    >
       {review.content}
       <Rating score={review.score} />
+      {showDeleteButton && (
+        <div
+          className="cursor-pointer absolute h-4 w-4 right-0 top-0 text-xs text-center"
+          onClick={() => handleClickedDeleteButton()}
+        >
+          X
+        </div>
+      )}
     </div>
   );
 }
--- myreact37/src/components/ReviewList.jsx
+++ myreact37/src/components/ReviewList.jsx
@@ -4,13 +4,19 @@ import { useState } from 'react';
 import ReviewForm from './ReviewForm';
 
 const INITIAL_STATE = [
-  { content: '스파이더맨 1 부터 본 사람이면 재미없을 수가 없다', score: 5 },
   {
+    id: 1,
+    content: '스파이더맨 1 부터 본 사람이면 재미없을 수가 없다',
+    score: 5,
+  },
+  {
+    id: 2,
     content:
       '전 스파이더맨 두명이 자책했던 일들이 여기서 어느정도 구원받았다는거에 감사드립니다',
     score: 4,
   },
   {
+    id: 3,
     content:
       '엔드게임을 뛰어넘는 영화가 죽기전에 나올까 생각했었는데.. 2년만에 나왔습니다',
     score: 5,
@@ -26,20 +32,34 @@ function ReviewList() {
   const [isShowReviewForm, setIsShowReviewForm] = useState(false);
 
   const saveReview = () => {
-    const review = { ...fieldValues };
+    // DB에 저장할 경우, DB에서 식별자(pk)를 제공해줍니다.
+    const randomId = `review-${new Date().getTime()}`;
+
+    const review = { ...fieldValues, id: randomId };
     setReviewList((prevReviewList) => [...prevReviewList, review]);
     clearFieldValues();
     setIsShowReviewForm(false);
   };
 
+  const deleteReview = (deletingReview) => {
+    // review에 유일한 식별자 id 속성이 있습니다.
+    setReviewList((prevReviewList) =>
+      prevReviewList.filter((review) => {
+        return review.id !== deletingReview.id;
+      }),
+    );
+  };
+
   return (
     <div>
-      <h2 className="text-xl mb-2 border-b-2 border-orange-500">ReviewList</h2>
+      <h2 className="text-xl mt-3 mb-2 border-b-2 border-orange-500">
+        ReviewList
+      </h2>
 
       {!isShowReviewForm && (
         <button
           onClick={() => setIsShowReviewForm(true)}
-          className="bg-orange-200 hover:bg-orange-400 p-1 rounded text-sm"
+          className="bg-orange-200 hover:bg-orange-400 p-1 rounded text-sm hover:-translate-y-1"
         >
           New Review
         </button>
@@ -58,7 +78,7 @@ function ReviewList() {
         </div>
       )}
       {reviewList.map((review) => (
-        <Review review={review} />
+        <Review review={review} handleDelete={deleteReview} />
       ))}
     </div>
   );
