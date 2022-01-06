c8115c2f9f302351234f00dd67d0a8efcebe4119
Chinseok Lee <me@askcompany.kr>
2022-01-06 16:20:19 +0900

    [myreact37] react-router@6를 활용한 TopNav 구현

--- myreact37/src/App.js
+++ myreact37/src/App.js
@@ -1,14 +1,23 @@
 // import PageCounter from 'pages/PageCounter';
 // import PageTodoList from 'pages/PageTodoList';
+import TopNav from 'components/TopNav';
+import PageCounter from 'pages/PageCounter';
 import PageReviewList from 'pages/PageReviewList';
+import PageTodoList from 'pages/PageTodoList';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className='app'>
-      {/* <PageCounter /> */}
-      {/* <PageTodoList /> */}
-      <PageReviewList />
+      <TopNav />
+
+      <Routes>
+        <Route path="/" element={<div>대문</div>} />
+        <Route path="/counter" element={<PageCounter />} />
+        <Route path="/reviews" element={<PageReviewList />} />
+        <Route path="/todos" element={<PageTodoList />} />
+      </Routes>
     </div>
   );
 }
---
+++ myreact37/src/components/TopNav.jsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+
+function TopNav() {
+  return (
+    <div className="my-3">
+      <ul className="flex gap-4">
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/todos">Todos</NavLink>
+        </li>
+        <li>
+          <NavLink to="/reviews">Reviews</NavLink>
+        </li>
+        <li>
+          <NavLink to="/counter">Counter</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function NavLink({ to, children }) {
+  return (
+    <Link
+      to={to}
+      className="pb-1 border-red-400 hover:border-b-4 hover:text-red-400 duration-75 text-sm"
+    >
+      {children}
+    </Link>
+  );
+}
+
+export default TopNav;
