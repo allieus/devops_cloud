0d2dcdb674f116d856f2bd4fd4f3d87578119673
Chinseok Lee <me@askcompany.kr>
2022-01-06 13:44:54 +0900

    [myreact37] Review 컴포넌트에 수정/삭제 이벤트 핸들러 속성값 추가

--- myreact37/src/components/Review.jsx
+++ myreact37/src/components/Review.jsx
@@ -1,8 +1,31 @@
 import { useState } from 'react';
 import Rating from './Rating';
 
-function Review({ review }) {
+// review : 보여줄 review 객체
+// handleEdit : 인자없는 함수. 수정 버튼 클릭 시에 호출.
+// handleDelete : 인자없는 함수. 삭제 버튼 클릭 시에 호출.
+
+function Review({ review, handleEdit, handleDelete }) {
   const [showMenu, setShowMenu] = useState(false);
+
+  const handleClickedEditButton = () => {
+    if (handleEdit) {
+      handleEdit();
+    } else {
+      console.warn('[Review] handleEdit 속성값이 지정되지 않았습니다.');
+    }
+  };
+
+  const handleClickedDeleteButton = () => {
+    // handleDelete 속성값이 없을 때에 대한 안내가 없기에, 비추천
+    // handleDelete && handleDelete();
+
+    if (handleDelete) {
+      handleDelete();
+    } else {
+      console.warn('[Review] handleDelete 속성값이 지정되지 않았습니다.');
+    }
+  };
 
   return (
     <div
@@ -12,10 +35,16 @@ function Review({ review }) {
     >
       {showMenu && (
         <div className="text-xs absolute top-0 right-0">
-          <span className="text-green-700 hover:bg-green-300 cursor-pointer mr-1">
+          <span
+            onClick={handleClickedEditButton}
+            className="text-green-700 hover:bg-green-300 cursor-pointer mr-1"
+          >
             수정
           </span>
-          <span className="text-red-700 hover:bg-red-300 cursor-pointer">
+          <span
+            onClick={handleClickedDeleteButton}
+            className="text-red-700 hover:bg-red-300 cursor-pointer"
+          >
             삭제
           </span>
         </div>
